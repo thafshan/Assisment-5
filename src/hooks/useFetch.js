70be9d64ea070beb37982b87966f4c6d5c8d7747
thafshan <afshankhanyt0004@gmail.com>
2025-11-27 04:18:07 +0530

    new code

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,35 +1,37 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 
-const useFetch = (url) => {
+// imported useState and useEffect to store data , loading , error and to run code when component doads or URL changes 
+
+function useFetch(url) {
+  // this is where i defined the hook , this is how a custom hook is created named as useFetch
+  // this recives an argument "url" which is API present in the products.jsx file
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+// these are all the state variables
+  useEffect(() => {
 
-  const fetchData = useCallback(async () => {
-    setLoading(true);
-    setError(null);
-
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error("Failed to fetch");
+    const fetchData = async () => {
+      setLoading(true);
+      try { 
+        // try func to fetch the API
+        const res = await fetch(url);
+        const json = await res.json();
+        setData(json);
+      } catch (err) {
+        setError("Error fetching data");
+      } finally {
+        setLoading(false);
       }
+    };
 
-      const result = await response.json();
-      setData(result);
+    fetchData();
 
-    } catch (err) {
-      setError(err.message);
-    } finally {
-      setLoading(false);
-    }
   }, [url]);
 
-  useEffect(() => {
-    fetchData();
-  }, [fetchData]);
-
   return { data, loading, error };
-};
+}
 
 export default useFetch;
+
+//  iam again assuring that AI was used but not to copy paste the code but to my understandings . 
